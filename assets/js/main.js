75d11c61fab85a6604caef31f0ef59dfe55716f3
minhal52 <minhalhanief@gmail.com>
2024-08-05 23:44:09 +0530

    changes updated on service page

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,13 +41,7 @@ workObserver.observe(workSection);
 
 
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // when the scroll is greater than the viewport height add this 
-    if(this.scrollY >= 50) header.classList.add('scroll-header');
-}
-window.addEventListener('scroll', scrollHeader)
+
 
 /*=============== SWIPER POPULAR ===============*/
 var swiperPopular = new Swiper(".popular-container", {
@@ -63,35 +57,6 @@ var swiperPopular = new Swiper(".popular-container", {
     },
   });
 
-/*=============== VALUE ACCORDION ===============*/
-const accordianItems = document.querySelectorAll('.value-accordian-item')
-
-accordianItems.forEach((item) =>{
-  const accordianHeader = item.querySelector('.value-accordian-header')
-  accordianHeader.addEventListener('click', ()=>{
-const openItem = document.querySelector('.accordian-open')
-
-toggleItem(item)
-
-if(openItem && openItem!== item){
-  toggleItem(openItem)
-}
-
-})
-
-})
-
-const toggleItem = (item) =>{
-  const accordianContent = item.querySelector('.value-accordian-content')
-
-if(item.classList.contains('accordian-open')){
-  accordianContent.removeAttribute('style')
-  item.classList.remove('accordian-open')
-}else{
-  accordianContent.style.height = accordianContent.scrollHeight + 'px'
-  item.classList.add('accordian-open')
-}
-}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
@@ -123,41 +88,11 @@ if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');else scrollUp.clas
 }
 window.addEventListener('scroll',scrollUp)
 
-/*=============== DARK LIGHT THEME ===============*/ 
-// const themeButton = document.getElementById('theme-button')
-// const darkTheme = 'dark-theme'
-// const iconTheme = 'bx-sun'
-
-// // Previously selected topic (if user selected)
-// const selectedTheme = localStorage.getItem('selected-theme')
-// const selectedIcon = localStorage.getItem('selected-icon')
-
-// // We obtain the current theme that the interface has by validating the dark-theme class
-// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
-
-// // We validate if the user previously chose a topic
-// if (selectedTheme) {
-//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-//   themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
-// }
-
-// // Activate / deactivate the theme manually with the button
-// themeButton.addEventListener('click', () => {
-//     // Add or remove the dark / icon theme
-//     document.body.classList.toggle(darkTheme)
-//     themeButton.classList.toggle(iconTheme)
-//     // We save the theme and the current icon that the user chose
-//     localStorage.setItem('selected-theme', getCurrentTheme())
-//     localStorage.setItem('selected-icon', getCurrentIcon())
-// })
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr= ScrollReveal({
   origin :'top',
   distance : '60px',
-  duration :2500,
+  duration :2000,
   // delay :400, 
   // reset: true 
 })
